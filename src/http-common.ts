50b68b821d17860b4604660ffc0072fe396230bd
mertbarut <mbarut@icloud.com>
2022-05-22 21:16:31 +0200

    changed the api to use heroku; prepared the files for deploying to github-pages

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://mbarut-absence-manager-api.herokuapp.com/',
   headers: {
     'Content-type': 'application/json'
   }
